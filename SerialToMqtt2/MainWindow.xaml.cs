3d401bea695aaf09f46027c78a22b7e8e0bbd4b7
Mike Partain <spiked3@gmail.com>
2015-04-19 02:41:45 -0700

    rename

--- SerialToMqtt2/MainWindow.xaml.cs
+++ SerialToMqtt2/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace SerialToMqtt2
         public ObservableCollection<ComportItem> ComPortItems { get; set; }
 
         private string[] CompPortsToMonitor = { "com3", "com4", "com14", "com15" };
-        private int[] ComPortsBaud = { 9600, 115200, 9600, 9600 };
+        private int[] ComPortsBaud = { 115200, 115200, 9600, 9600 };
 
         private Dictionary<string, List<SerialPort>> TopicListeners = new Dictionary<string, List<SerialPort>>();
         private Dictionary<SerialPort, ComportItem> ComPortItemsDictionary = new Dictionary<SerialPort, ComportItem>();
